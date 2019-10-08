e2ebc2b7cc9b550e54a0d4f2db3044fc79766216
xinru <s3a632065@student.ncut.edu.tw>
2019-10-09 03:41:04 +0800

    將目前目錄所有發生變動的檔案加入git處理

---
+++ composer-lab-assets/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_PORT', 3306);
+define('DB_DATABASE', 'lara_0804_p');
+define('DB_CHARSET', 'utf8');
+define('DB_USERNAME', 'xinru');
+define('DB_PASSWORD', 's3a632065@student.ncut.edu.tw');
